92c88b78a9c5fd706c7b0618978916dd3a5d7bcb
Metadata Generator <metadatagenerator@fakemail.com>
2025-05-13 16:32:11 +0000

    New preview v1.21.90.25

--- script-declerations/@minecraft/diagnostics_1.0.0-beta.d.ts
+++ script-declerations/@minecraft/diagnostics_1.0.0-beta.d.ts
@@ -1,10 +1,11 @@
 import * as _1e from '@minecraft/common';
+import * as _2c from '@minecraft/server-admin';
 
 // Enums - 1
 export enum SentryBreadcrumbLevel { debug = "debug", error = "error", info = "info", warning = "warning"};
 
 // Interfaces - 1
-export interface SentryOptions { dsn: string};
+export interface SentryOptions { debug?: boolean; dsn: _2c.SecretString | string; maxBreadcrumbs?: number; sampleRate?: number};
 
 // Classes - 1
 export class Sentry { public addBreadcrumb(level: SentryBreadcrumbLevel, message: string, category?: string): void; public addTag(name: string, value: string): void; public getTags(): Record<string,string>; public init(options: SentryOptions): void; public removeTag(name: string): void; private constructor();};
