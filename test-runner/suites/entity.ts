34c5294fb55d86ae2476aac0829fbaf7e6b02d4e
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-06-13 09:23:31 +0300

    Check for is_baked

--- test-runner/suites/entity.ts
+++ test-runner/suites/entity.ts
@@ -5,4 +5,5 @@ TestSuite.withSetup('entity', () => spawnEntity('minecraft:cow'))
    .test(entity => entity.typeId)
    .test(entity => entity.location)
    .test(entity => entity.localizationKey)
-   .test(entity => entity.getComponents());
+   .test(entity => entity.getComponents())
+   .test(entity => JSON.stringify(entity));
