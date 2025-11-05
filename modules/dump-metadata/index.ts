1796972fa5967491f15145a2e2ef5c4046550d2f
con <conmaster@noreply.com>
2025-11-05 20:44:34 +0100

    Tests 13

--- modules/dump-metadata/index.ts
+++ modules/dump-metadata/index.ts
@@ -13,6 +13,7 @@ export default class Metadata {
         const process = await installation.runWithTestConfig({
             generate_all: true,
         }, null);
+        process.enabledOutputRedirection();
         process.stop(true, BDS_PROCESS_MAX_LIFE_TIME); //15s should more more than good
         const result = await process.wait().catch(_ => (console.error(_), null));
         if (result === null) return -1;
