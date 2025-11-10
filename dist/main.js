a80ea8628bd12939baaf0cdda8cd99a2b83766c5
con <conmaster@noreply.com>
2025-11-10 13:45:31 +0100

    Tests 35

--- dist/main.js
+++ dist/main.js
@@ -910,7 +910,10 @@ var GithubUtils = class {
 //#endregion
 //#region modules/utils/general.ts
 function getEngineVersion(fullVersion) {
-	return fullVersion.split(".").slice(0, 3).join(".");
+	let versions = fullVersion.split(".").slice(0, 3);
+	let num = Number(versions.at(-1));
+	if (isFinite(Number(versions.at(-1)))) versions[versions.length - 1] = String(Math.floor(num / 10) * 10);
+	return versions.join(".");
 }
 
 //#endregion
--- modules/utils/general.ts
+++ modules/utils/general.ts
@@ -1,3 +1,8 @@
 export function getEngineVersion(fullVersion: string): string{
-    return fullVersion.split(".").slice(0, 3).join(".");
+    let versions = fullVersion.split(".").slice(0, 3);
+    let num = Number(versions.at(-1));
+    if(isFinite(Number(versions.at(-1))))
+        versions[versions.length - 1] = String((Math.floor(num/10))*10);
+
+    return versions.join(".");
 }
